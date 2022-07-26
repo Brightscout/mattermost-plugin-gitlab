fc262380c3a0742f75058a138a7a519fedda10ff
ayusht2810 <ayush.thakur@joshtechnologygroup.com>
2022-07-26 17:55:13 +0530

    [MI-1957] Correct comment

--- webapp/src/components/sidebar_right/index.tsx
+++ webapp/src/components/sidebar_right/index.tsx
@@ -127,7 +127,7 @@ function SidebarRight({theme}: {theme: Theme}) {
         };
     });
 
-    // States used for store the updated details of PRs, reviews, and RHSState after taking from redux store.
+    // States used for storing the updated details of PRs, reviews, and RHSState after being taken from the redux store.
     const [updatedPrs, setUpdatedPrs] = useState<Item[]>(yourPrs);
     const [RHSState, setRHSState] = useState('');
     const [updatedReviews, setUpdatedReviews] = useState<Item[]>(reviews);
